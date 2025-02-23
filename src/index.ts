c40783bfb929f77259ac6a0f1eccc63ac9805c4b
Yifeng Wang <git@yifengwang.me>
2025-02-23 16:45:37 -0500

    Check winning condition

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ type Board = [
   [Cell, Cell, Cell],
   [Cell, Cell, Cell]
 ]
+type Winner = Cell | "DRAW"
 
 let boardState: Board = [
   ["", "", ""],
@@ -18,6 +19,7 @@ let boardState: Board = [
   ["", "", ""]
 ];
 let currentMove: "X" | "O" = "X";
+let winner: Winner;
 
 function createCell(row: number, col: number, content: Cell = "") {
   const cell = document.createElement("button");
@@ -27,16 +29,77 @@ function createCell(row: number, col: number, content: Cell = "") {
   cell.classList.add("cell");
 
   cell.addEventListener("click", () => {
+    if (winner) {
+      return;
+    }
 
     if (boardState[row][col] === "") {
       boardState[row][col] = currentMove
       currentMove = currentMove === "X" ? "O" : "X"
 
+      winner = checkWinner()
       renderBoard()
     }
   })
 
   return cell;
+}
+
+function checkWinner(): Winner {
+  // Check rows
+  for (let i = 0; i < ROW_COUNT; i++) {
+    const cells: Cell[] = []
+    for (let j = 0; j < COL_COUNT; j++) {
+      cells.push(boardState[i][j])
+    }
+    if (checkWinning(cells)) {
+      return cells[0]
+    }
+  }
+
+  // Check columns
+  for (let j = 0; j < COL_COUNT; j++) {
+    const cells: Cell[] = []
+    for (let i = 0; i < ROW_COUNT; i++) {
+      cells.push(boardState[i][j])
+    }
+    if (checkWinning(cells)) {
+      return cells[0]
+    }
+  }
+
+  // Check diagonal
+  let cells: Cell[] = []
+  for (let i = 0; i < ROW_COUNT; i++) {
+    cells.push(boardState[i][i])
+  }
+
+  if (checkWinning(cells)) {
+    return cells[0]
+  }
+
+  cells = []
+  for (let j = 0; j < COL_COUNT; j++) {
+    cells.push(boardState[ROW_COUNT - j - 1][j])
+  }
+
+  if (checkWinning(cells)) {
+    return cells[0]
+  }
+
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COL_COUNT; j++) {
+      if (boardState[i][j] === "") {
+        return ""
+      }
+    }
+  }
+
+  return "DRAW"
+}
+
+function checkWinning(cells: Cell[]): boolean {
+  return cells[0] !== "" && cells.every(cell => cell === cells[0])
 }
 
 function renderBoard() {
@@ -54,7 +117,7 @@ function renderBoard() {
   }
   const moveElement = document.createElement("p");
   moveElement.id = "move-element";
-  moveElement.innerText = `Next Move: ${currentMove}`;
+  moveElement.innerText = winner ? `Winner: ${winner}` : `Next Move: ${currentMove}`;
   moveElement.classList.add("current-move");
   appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
@@ -69,6 +132,7 @@ function init() {
       ["", "", ""]
     ];
     currentMove = "X";
+    winner = "";
     renderBoard();
   });
   renderBoard();
